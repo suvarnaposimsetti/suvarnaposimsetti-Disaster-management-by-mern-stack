320771e9cd71b67cec00ef411f24d63b79f5fb3a
suvarna posimsetti <139966316+suvarnaposimsetti@users.noreply.github.com>
2025-06-17 21:39:44 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
